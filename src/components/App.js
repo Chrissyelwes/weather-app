b403b16c66d76289662d007cc01f9f791b3b4f87
Chrissy Elwes <96088605+Chrissyelwes@users.noreply.github.com>
2022-05-31 15:27:21 +0100

    Location details rendered

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
 // import "./App.css";
 import "../styles/App.css";
 import React from "react";
+import PropTypes from "prop-types";
+import LocationDetails from "./LocationDetails";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Weather App</h1>
-    </div>
-  );
+function App({ location }) {
+  const { city, country } = location;
+  return <LocationDetails city={city} country={country} />;
 }
+
+App.propTypes = {
+  location: PropTypes.shape({
+    city: PropTypes.string,
+    country: PropTypes.string,
+  }).isRequired,
+};
 
 export default App;
